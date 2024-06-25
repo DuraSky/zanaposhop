1db46c4f798924ab39a13e4ba1df431d494f346c
Lukas Broskovic <lukas.broskovic@gmail.com>
2024-06-25 09:16:34 +0200

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ["www.zanapo.cz", "placehold.jp"], // Add your external image domains here
+  },
+};
 
 export default nextConfig;
